1314eee980651f3438bd9d214e4133bfb918aa40
Charles Davis <creepervenom10@gmail.com>
2021-03-07 17:55:35 -0700

    Create TableController

---
+++ src/controllers/TableController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class TableController
+{
+
+	public TableController()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
